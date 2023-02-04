a8f81ef41d0716f69fcf6bc0a4fc18e65edf1644
lordPratish <55389359+lordPratish@users.noreply.github.com>
2023-02-04 18:18:53 +0200

    Update leet.py

--- leet.py
+++ leet.py
@@ -1,54 +1,49 @@
-def leet_speak(word):
-    word = word.lower()
-    word = word.replace("a", "4")
-    word = word.replace("e", "3")
-    word = word.replace("g", "6")
-    word = word.replace("i", "1")
-    word = word.replace("o", "0")
-    word = word.replace("s", "5")
-    word = word.replace("t", "7")
-    return word.capitalize()
+import itertools
 
-def word_variations(word):
-    variations = [word, word.upper(), word.capitalize()]
-    word = word.lower()
-    variations.extend([
-        word + "123",
-        word + "!@#",
-        word + "xyz",
-        word + "2019"
-    ])
-    return variations
+def leet_conversion(word):
+    leet_dict = {'a': '4', 'e': '3', 'g': '6', 'i': '1', 'o': '0', 's': '5', 't': '7'}
+    leet_word = ''
+    for char in word:
+        if char.lower() in leet_dict:
+            leet_word += leet_dict[char.lower()]
+        else:
+            leet_word += char
+    return leet_word
 
-def add_seasons(word):
-    seasons = ["Spring", "Summer", "Fall", "Winter"]
-    variations = [word + season for season in seasons]
-    return variations
-
-def add_gods(word):
-    gods = ["Zeus", "Jupiter", "Apollo", "Venus"]
-    variations = [word + god for god in gods]
-    return variations
+def join_words(word1, word2):
+    combined_words = [word1 + word2, word2 + word1]
+    combined_words = [leet_conversion(word) for word in combined_words]
+    return combined_words
 
 def main():
-    input_file = "input.txt"
-    output_file = "output.txt"
-    with open(input_file, "r") as f:
-        words = f.readlines()
-    words = [word.strip() for word in words]
-    leet_words = [leet_speak(word) for word in words]
+    with open('input.txt', 'r') as file:
+        words = [line.strip() for line in file]
+
+    leet_words = [leet_conversion(word) for word in words]
+
     combined_words = []
     for i in range(len(leet_words)):
         for j in range(i+1, len(leet_words)):
             combined_words.extend(join_words(leet_words[i], leet_words[j]))
-    single_word_variations = []
-    for word in leet_words:
-        single_word_variations.extend(word_variations(word))
-        single_word_variations.extend(add_seasons(word))
-        single_word_variations.extend(add_gods(word))
-    all_variations = combined_words + single_word_variations
-    with open(output_file, "w") as f:
-        f.write("\n".join(all_variations))
 
-if __name__ == "__main__":
+    seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+    gods = ['Zeus', 'Jupiter', 'Apollo', 'Venus']
+
+    with open('output.txt', 'w') as file:
+        for word in leet_words:
+            file.write(word + '\n')
+            file.write(word.capitalize() + '\n')
+        for word in combined_words:
+            file.write(word + '\n')
+            file.write(word.capitalize() + '\n')
+        for season in seasons:
+            for word in leet_words:
+                file.write(season + word + '\n')
+                file.write(season + word.capitalize() + '\n')
+        for god in gods:
+            for word in leet_words:
+                file.write(god + word + '\n')
+                file.write(god + word.capitalize() + '\n')
+
+if __name__ == '__main__':
     main()
